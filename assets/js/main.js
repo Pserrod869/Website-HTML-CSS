0a9348b78972b9a9ec9cd31e80905fc63446c760
Pserrod869 <pserrod869@g.educaand.es>
2025-12-15 22:43:23 +0100

    Section popular finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,12 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== SWIPER POPULAR ===============*/
-
+const swiperPopular = new Swiper('.popular__swiper', {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+});
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
